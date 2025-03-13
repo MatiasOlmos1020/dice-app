3c45a4cc75f9b2d3288718a863a3d97e024a97be
Matias Olmos <matiasolmosrivero@gmail.com>
2025-03-13 12:20:25 -0300

    Minimal dice-app 0.1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
 import { DiceDisplayComponent } from './Components/dice-display/dice-display.component';
 import { DiceSelectComponent } from './Components/dice-select/dice-select.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-<<<<<<< HEAD
   selector: 'app-root',
   imports: [RouterOutlet],
   standalone: true,
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {}
-=======
-  selector: 'app-root>',
-  imports: [DiceDisplayComponent,DiceSelectComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  menuHidden = true;
 
-  toggleMenu() {
-    this.menuHidden = !this.menuHidden;
-  }
-}
->>>>>>> 584e31766b57888bea5e1daa9cc813b5dd1343a1
